4c5a8325ec78b640c0727b08a0d825f06cd0fc17
Rifki Fajar Ramadhani <rifkid56@gmail.com>
2024-05-02 09:59:17 +0700

    add category repository, error helper

---
+++ go.mod
@@ -0,0 +1,3 @@
+module golang-restfulapi
+
+go 1.22.2
---
+++ repository/category_repository.go
@@ -0,0 +1,67 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"golang-restfulapi/entity"
+	"golang-restfulapi/helper"
+)
+
+type ICategoryRepository interface {
+	Create(ctx context.Context, tx *sql.Tx, c entity.Category) entity.Category
+	Update(ctx context.Context, tx *sql.Tx, c entity.Category) entity.Category
+	Delete(ctx context.Context, tx *sql.Tx, c entity.Category)
+	Get(ctx context.Context, tx *sql.Tx) []entity.Category
+	GetById(ctx context.Context, tx *sql.Tx, id int) (entity.Category, error)
+}
+
+type CategoryRepository struct {
+}
+
+func (r *CategoryRepository) Create(ctx context.Context, tx *sql.Tx, c entity.Category) entity.Category {
+	query := "insert into categories(name) values(?)"
+	result, err := tx.ExecContext(ctx, query, c.Name)
+	helper.PanicIfError(err)
+
+	id, err := result.LastInsertId()
+	helper.PanicIfError(err)
+
+	c.Id = int(id)
+	return c
+}
+
+func (r *CategoryRepository) Update(ctx context.Context, tx *sql.Tx, c entity.Category) entity.Category {
+	query := "update categories set name = ? where id = ?"
+	_, err := tx.ExecContext(ctx, query, c.Name, c.Id)
+	helper.PanicIfError(err)
+
+	return c
+}
+
+func (r *CategoryRepository) Delete(ctx context.Context, tx *sql.Tx, c entity.Category) {
+	query := "delete from categories where id = ?"
+	_, err := tx.ExecContext(ctx, query, c.Id)
+	helper.PanicIfError(err)
+}
+
+func (r *CategoryRepository) Get(ctx context.Context, tx *sql.Tx, c entity.Category) []entity.Category {
+	return c
+}
+
+func (r *CategoryRepository) GetById(ctx context.Context, tx *sql.Tx, id int) (entity.Category, error) {
+	query := "select id, name from categories where id = ?"
+	result, err := tx.QueryContext(ctx, query, id)
+	helper.PanicIfError(err)
+
+	category := entity.Category{}
+	if result.Next() {
+		err := result.Scan(&category.Id, &category.Name)
+		helper.PanicIfError(err)
+
+		return category, nil
+	} else {
+		return category, errors.New("cateogry not found")
+	}
+
+}
